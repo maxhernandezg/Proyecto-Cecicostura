219c6faaf8b84318278adab88b6ba0509c18030d
Maximiliano Hernández <hernandez.gallardo81@gmail.com>
2024-06-24 10:48:40 -0400

    Se crean Modelos y Migraciones para Users y Services, y se corrigen URLs del Navbar
    Se crean Modelos de las siguientes aplicaciones:
    -> Users:
      - User
      - UserType
    
    -> Services:
      - Service
    
    Se modifica la URL del Navbar para que 'Inicio' y el 'Logo' redireccionen correctamente a Index.html.

--- services/models.py
+++ services/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_path = models.CharField(max_length=255, blank=True, null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class UserType(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=100)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    user_type = models.OneToOneField(UserType, on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
