4d5721f3a5777f834864b668061b20f2e65ea8e3
Maximiliano Hernández <hernandez.gallardo81@gmail.com>
2024-07-16 16:32:48 -0400

    Proyecto terminado

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
 
+# Definición de las rutas URL para las vistas del carrito
 urlpatterns = [
+    # Ruta para ver el carrito de compras
     path('carrito/', views.carrito, name='carrito'),
+
+    # Ruta para agregar un servicio al carrito
     path('add_to_cart/<int:service_id>/', views.add_to_cart, name='add_to_cart'),
+
+    # Ruta para eliminar un servicio del carrito
     path('remove_from_cart/<int:service_id>/', views.remove_from_cart, name='remove_from_cart'),
+
+    # Ruta para proceder al pago vía WhatsApp
     path('whatsapp_checkout/', views.whatsapp_checkout, name='whatsapp_checkout'),
 ]
